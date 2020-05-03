72e929e1564bd28c57965db04659af90a7fdf49a
Andrés Ayala <53452577+Tero-AA@users.noreply.github.com>
2020-05-02 21:23:11 -0500

    Navbar Completed

--- scripts/changeTheme.js
+++ scripts/changeTheme.js
@@ -1,5 +1,18 @@
 const body = document.getElementById('body');
+const dropdown = document.getElementById('dropdown-menu');
+let open = false;
 
+// Change Theme
 let changeTheme = theme => {
   body.setAttribute('class', `theme-${theme}`);
+};
+
+// Open Dropdown
+const openDropdown = () => {
+  open = !open;
+  if (open) {
+    dropdown.setAttribute('style', 'display: flex');
+  } else {
+    dropdown.setAttribute('style', 'display: none');
+  }
 };
